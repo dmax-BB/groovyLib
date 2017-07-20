8f5524d61ec97b304dcaf8af9fad2409a0ad00a6
Dana Maxfield <dmaxfield@blackducksoftware.com>
2017-07-20 16:46:57 +0000

    Add if to allow for better automation

--- src/com/danasoftware/jenkins/pipeline/libraries/SourceClone.groovy
+++ src/com/danasoftware/jenkins/pipeline/libraries/SourceClone.groovy
@@ -1,17 +1,21 @@
 package com.danasoftware.jenkins.pipeline.libraries
 
 public void cloneSourceCode(def targetUrl, def targetDir, def credentialsID, def branchName = "origin/master", def scmClass = "GitSCM"){
+  if (branchName=="" || branchName==null || branchName=="null") {
+    branchName="origin/master"
+  }
+
   return {
     node {
       try {
-        println "Starting clone for ${targetDir} at " + new Date()
+        println "Starting clone for ${targetDir}:${branchName} at " + new Date()
         checkout scm: [
                  $class: "${scmClass}",
                  branches: [[name: "${branchName}"]],
                  userRemoteConfigs: [[credentialsId: "${credentialsID}", url: "${targetUrl}"]],
                  extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${targetDir}"]]
                  ]
-        println "Finished clone for ${targetDir} at " + new Date()
+        println "Finished clone for ${targetDir}:${branchName} at " + new Date()
       } catch (Exception e){
         throw e
       }
